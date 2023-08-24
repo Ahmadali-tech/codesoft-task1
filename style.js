0a91bf223eef4f0100ca9559d9eca7dcbac4ee26
AHMAD ALI <ahmadalihafeez@gmail.com>
2023-08-24 01:05:44 -0700

    a new feature is add

---
+++ style.js
@@ -0,0 +1,8 @@
+var type =new type(".text",{
+    Strings:["frontened webdeveloper", "webdesigner", "logodesigner "],
+    backspeed:100,
+    typespeed:100,
+    backdelay:1000,
+    loop:true,
+
+});
